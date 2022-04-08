83124e760fd618e6efe7f7a7fe72de84e5a2d3db
Daniel Elkington <20923693+danielelkington@users.noreply.github.com>
2022-04-08 14:26:12 +0800

    Get moving working

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,11 @@ import { computed } from '@vue/reactivity';
 import {ref, reactive} from 'vue'
 import Wall from './Wall.vue'
 import Treasure from './Treasure.vue'
-const S = 'S'
-const W = 'W'
-const T = 'T'
-const _ = '_'
-const P = 'P'
+const S = 'S' // Start
+const W = 'W' // Wall
+const T = 'T' // Treasure
+const _ = '_' // Nothing
+const P = 'P' // Player
 const rooms = reactive([
   // Room 1
   [
@@ -29,8 +29,14 @@ function getPositionOf(thing) {
     return -1
   }
 }
-const playerPosition = ref(getPositionOf(S))
-const playerFacing = ref('right') // right, left, up, down
+// State = playerPosition + playerFacing + playerColour.
+const playerStates = reactive([{position: getPositionOf(S), facing: 'right', colour: 'red', collision: null}])
+const playerStateIndex = ref(0)
+const playerCurrentState = computed(() => playerStates[playerStateIndex.value])
+const playerLastState = computed(() => playerStates[playerStates.length - 1])
+const playerPosition = computed(() => playerCurrentState.value.position)
+const playerFacing = computed(() => playerCurrentState.value.facing)
+const playerColour = computed(() => playerCurrentState.value.colour)
 const rotatePlayer = computed(() => {
   switch(playerFacing.value) {
     case 'left': return 0;
@@ -44,13 +50,112 @@ const flattenedRoomElements = computed(() => {
   const elements = []
   for (let i = 0; i < selectedRoom.value.length; i++) {
     for (let j = 0; j < selectedRoom.value[i].length; j++) {
-      if (playerPosition.value[0] === i && playerPosition.value[0] === j) {
+      if (playerPosition.value[0] === i && playerPosition.value[1] === j) {
         elements.push(P)
       } else elements.push(selectedRoom.value[i][j])
     }
   }
   return elements
 })
+
+// TODO - when we run through the code, generate new states, then 
+// gradually move through each state to play the game.
+// Try to animate the player!
+
+const move = function(direction) { // forward or backward
+  const currentPosition = playerLastState.value.position
+  const movementDirection = direction === 'forward'
+    ? playerLastState.value.facing
+    : (playerLastState.value.facing === 'right' ? 'left'
+      : playerLastState.value.facing === 'left' ? 'right'
+      : playerLastState.value.facing === 'up' ? 'down'
+      : 'up'
+    )
+  let targetPosition
+  switch(movementDirection) {
+    case 'right':
+      targetPosition = [currentPosition[0], currentPosition[1] + 1]
+      break
+    case 'up':
+      targetPosition = [currentPosition[0] - 1, currentPosition[1]]
+      break
+    case 'down':
+      targetPosition = [currentPosition[0] + 1, currentPosition[1]]
+      break
+    case 'left':
+      targetPosition = [currentPosition[0], currentPosition[1] - 1]
+  }
+  // Off the board?
+  if (targetPosition[0] < 0 ||
+    targetPosition[0] >= selectedRoom.value.length ||
+    targetPosition[1] < 0 ||
+    targetPosition[1] >= selectedRoom.value[0].length) {
+    playerStates.push({...playerLastState.value, collision: movementDirection})
+  } else {
+    const targetPositionValue = selectedRoom.value[targetPosition[0]][targetPosition[1]]
+    if (targetPositionValue === W) {
+      // Wall
+      playerStates.push({...playerLastState.value, collision: movementDirection})
+    } else {
+      // Move the player
+      const newState = {...playerLastState.value, position: targetPosition, collision: null}
+      // Reached the treasure?
+      if (targetPositionValue === T) {
+        newState.won = true
+        gameInProgress = false
+      }
+      playerStates.push(newState)
+    }
+  }
+}
+
+const turn = function(direction) { // left or right
+  let newDirection
+  if (direction === 'left') {
+    switch (playerLastState.value.facing) {
+      case 'left':
+        newDirection = 'down'
+        break
+      case 'up':
+        newDirection = 'left'
+        break
+      case 'right':
+        newDirection = 'up'
+        break
+      case 'down':
+        newDirection = 'right'
+        break
+    } 
+  } else {
+    switch(playerLastState.value.facing) {
+      case 'left':
+        newDirection = 'up'
+        break
+      case 'up': 
+        newDirection = 'right'
+        break
+      case 'right': 
+        newDirection = 'down'
+        break
+      case 'down':
+        newDirection = 'left'
+        break
+    }
+  }
+  playerStates.push({...playerLastState.value, facing: newDirection, collision: null})
+}
+
+defineExpose({
+  move,
+  turn
+})
+
+setInterval(() => {
+  if (playerStateIndex.value < playerStates.length - 1) {
+    playerStateIndex.value++
+    console.log(playerCurrentState.value.position)
+  }
+}, 1000)
 </script>
 
 <template>
@@ -62,12 +167,13 @@ const flattenedRoomElements = computed(() => {
     <div class="room-element" v-for="(element, i) in flattenedRoomElements" :key="i">
       <svg v-if="element === 'P'" class="player" viewBox="0 0 500 500"
         :style="{'transform': 'rotate(' + rotatePlayer + 'deg)'}">
-        <polygon points="0,250 500,40 500,460" style="fill:red;" />
+        <polygon points="0,250 500,40 500,460" :style="{fill:playerColour}" />
       </svg>
       <Wall v-if="element === 'W'" class="wall"/>
       <Treasure v-if="element === 'T'" class="treasure"/>
     </div>
   </div>
+  {{playerStates}}
 </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,4 +1,37 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+const component = app.mount('#app')
+
+// Global variables
+let gameInProgress = true
+let obstacleAhead = false
+let obstacleBehind = false
+
+function moveForward() {
+  component.move('forward')
+}
+
+function moveBackward() {
+  component.move('backward')
+}
+
+function turnLeft() {
+  component.turn('left')
+}
+
+function turnRight() {
+  component.turn('right')
+}
+
+function changeColour(colour) {
+  component.changeColour(colour)
+}
+
+// turnRight()
+// moveForward()
+// moveForward()
+// turnLeft()
+// moveForward()
+// moveForward()
